c339e82021ff022ce9d10c0881a31c8ad93a2799
Arjun Erasani <arerasani9@gmail.com>
2026-05-22 12:11:11 -0400

    initial commit after project restructuring

---
+++ backend/src/test/java/com/arjunerasani/chat_system/ChatSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjunerasani.chat_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
